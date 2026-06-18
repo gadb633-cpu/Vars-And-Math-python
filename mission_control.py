1ce7b4de8e3faaba7ad13d041e2e65cf1c2e2e5d
gad-biton <gadb633@gmail.com>
2026-06-18 14:40:54 +0300

    Print each variable type

--- mission_control.py
+++ mission_control.py
@@ -3,3 +3,7 @@ mission_code = 654213
 distance_to_target = 18.5
 mission_active_status = True
 print(f"agent name:{agent_name}, mission code:{mission_code}, distance to target:{distance_to_target}, mission active status:{mission_active_status}")
+print(type(agent_name))
+print(type(mission_code))
+print(type(distance_to_target))
+print(type(mission_active_status))
