e6d3dba21ee20bcd08434fc8658cb4ca120df23d
gad-biton <gadb633@gmail.com>
2026-06-18 14:35:16 +0300

    added basid mission data

---
+++ mission_control.py
@@ -0,0 +1,5 @@
+agent_name = "jj"
+mission_code = 654213
+distance_to_target = 18.5
+mission_active_status = True
+print(f"agent name:{agent_name}, mission code:{mission_code}, distance to target:{distance_to_target}, mission active status:{mission_active_status}")
