71073c600e67d3cc1018cc32f2ea8882af9c3727
gad-biton <gadb633@gmail.com>
2026-06-18 21:57:21 +0300

    added boolean with bin

---
+++ Self_Learn_binary.py
@@ -0,0 +1,23 @@
+# Understanding Questions
+# Part 1
+# 1100-12
+# 110111-55
+# 11101-29
+# 11110110-(-10)
+# Part 2
+# 2 בחזקת 64 
+# Practice
+# Part 1
+five_string = "5"
+five_number = 5
+# print(bin(five_string))
+# print(bin(five_number))
+# There is an error because python "bin" is only about numbers.
+# Part 2
+x = True
+y = False
+print(bin(x))
+print(bin(y))
+print(x ==y)
+print(x > y)
+
